1f7de9006d1f47195f52db248b9a51c949c211b9
goodnessm3 <cthulhu3125@gmail.com>
2020-03-13 17:15:23 +0000

    custom number of thumbs for contact sheet

--- videoobject.py
+++ videoobject.py
@@ -9,28 +9,37 @@ from random import randint
 FFMPEG = SETTINGS["FFMPEG_PATH"]
 FFPROBE = SETTINGS["FFPROBE_PATH"]
 
+class BadVideoException(Exception):
+
+    pass
+
 
 class VideoObject:
 
     """Container for information about a video file. Generates thumbnails with ffmpeg and stores them, also
     gets general info like duration, resolution, etc, to be made available to the main program."""
 
-    def __init__(self, full_path):
+    def __init__(self, full_path, thumbnails=9):
+
+        """By default get 9 images for the GUI, but can ask for more when e.g. making contact sheets"""
 
         self.path = full_path
         filename = os.path.split(self.path)[1]
         self.filename = filename.rstrip("\"")  # filename final quote mark stripped off
-        self.duration, self.increment = self.get_initial_info(self.path)
+        self.duration, self.increment = self.get_initial_info(self.path, thumbnails)
         if self.duration is not None:
             # if it's none then this video has broken ffmpeg
-            self.time_points = self.get_time_points()
-            self.images = self.get_thumbnails(self.time_points)
+            self.time_points = self.get_time_points(thumbnails)
+            try:
+                self.images = self.get_thumbnails(self.time_points)
+            except BadVideoException:
+                raise BadVideoException(f"ffmpeg could not read the file at {full_path}")
 
     def refine(self):
 
         """apply a small delta to the thumbnail time points to get different images"""
 
-        tp = self.get_time_points()
+        tp = self.time_points
         tp2 = [randint(-5, 5) + x for x in tp]
         # check to make sure the random addition hasn't put the time outside of the video length
         if tp2[0] < 0:
@@ -41,7 +50,7 @@ class VideoObject:
         self.images = self.get_thumbnails(self.time_points)
 
     @staticmethod
-    def get_initial_info(path):
+    def get_initial_info(path, no_thumbnails):
 
         cmd = f'''{FFPROBE} -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "{path}"'''
         pipe = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE, bufsize=10 ** 8)
@@ -59,7 +68,7 @@ class VideoObject:
             print(error)
             return None, None
 
-        increment = duration / 10.0
+        increment = duration / float(no_thumbnails + 1)
 
         return duration, increment
 
@@ -97,21 +106,22 @@ class VideoObject:
             try:
                 image = Image.open(flo)
             except:
-                print("error opening image")
-                print(imagedata2[1])  # print ffmpeg's stderr to the console
-                return []
+                # print("error opening image")
+                # print(imagedata2[1])  # print ffmpeg's stderr to the console
+                raise BadVideoException
+
             images_to_return.append(image)
 
         return images_to_return
 
-    def get_time_points(self):
+    def get_time_points(self, number):
 
         """returns 9 time points within self.duration, evenly spread with no other arguments or centred around time"""
 
         a = 3.0
         times = []
 
-        while len(times) < 9:
+        while len(times) < number:
             times.append(a)
             a += self.increment
 
@@ -134,18 +144,28 @@ class VideoObject:
 
         """writes a 3x3 thumbnail sheet using the generated images to the given directory path"""
 
-        container = Image.new("RGB", (1000, 850))
+        a, b = divmod(len(self.images), 3)
+        height_needed = a * 250  # 240 px thumbnail height with 10 px gap
+        if not b == 0:
+            height_needed += 250  # extra row
+        height_needed += 150  # for title info
+        container = Image.new("RGB", (1000, height_needed))
         ctx = ImageDraw.Draw(container)
         font = ImageFont.truetype("arial.ttf", 20)
         font2 = ImageFont.truetype("arial.ttf", 16)
 
         it = iter(self.images)
         it2 = iter(["{}:{}".format(*self.timeconvert(x)) for x in self.time_points])
-        for y in (100, 350, 600):
+        y = 100  # vertical coordinate to start drawing
+        while y < height_needed - 250:
             for x in (10, 340, 670):
-                container.paste(next(it), (x,y))
-                ctx.rectangle((x, y, x + 45, y + 20), fill=(0, 0, 0))  # to write the timestamp text
-                ctx.text((x+1, y+1), next(it2), font=font2)
+                try:
+                    container.paste(next(it), (x,y))
+                    ctx.rectangle((x, y, x + 45, y + 20), fill=(0, 0, 0))  # to write the timestamp text
+                    ctx.text((x+1, y+1), next(it2), font=font2)
+                except StopIteration:
+                    break  # in case the number of thumbnails gives an incomplete row
+            y += 250
 
         ctx.text((10, 10), self.filename, font=font)
         extra_info = f'''{self.get_video_res(self.path)},  {"{}:{}".format(*self.timeconvert(self.duration))}'''
